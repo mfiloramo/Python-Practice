990a3858e9600cc9ec95587fdea316c154b98724
mfiloramo <mlfiloramo@gmail.com>
2021-06-29 01:33:07 -0400

    Added 1 new challenge (Leetcode).

--- Practice/challenges.py
+++ Practice/challenges.py
@@ -1378,3 +1378,10 @@ class Solution(object):
                 nums.remove(i)
 
         return len(nums)
+
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        for num in nums:
+            if num == val:
+                nums.remove(val)
+        return len(nums)
