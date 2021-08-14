7fd58d05eb8e1af5a52ab58d34b94dd222e3853c
mfiloramo <mlfiloramo@gmail.com>
2021-08-14 14:24:45 -0400

    Added longestprefix challenge.

--- Practice/challenges.py
+++ Practice/challenges.py
@@ -1414,3 +1414,20 @@ class Solution(object):
                 break
         return num
 
+class Solution(object):
+    def longestprefix(self, strs) -> str:
+        """Returns a string that indicates the longest common prefix within the given array."""
+        if len(strs) == 0:
+            return ""
+        current = strs[0]
+        for i in range(1, len(strs)):
+            temp = ""
+            if len(current) == 0:
+                break
+            for j in range(len(strs[i])):
+                if j < len(current) and current[j] == strs[i][j]:
+                    temp += current[j]
+                else:
+                    break
+            current = temp
+        return current
